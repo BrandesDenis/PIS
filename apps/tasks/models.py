2b1fc0f29e7b6a8e995f7f15088e978b1116d934
DEN <DEN>
2020-06-29 22:06:28 +0500

    Оформление задач

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,52 +1,36 @@
 from datetime import date
-from typing import NoReturn
 
 from django.db import models
+
+from apps.core.models import PARAGRAPHS
 
 
 class Task(models.Model):
 
-    paragraphs = (
-        (111, "111"),
-        (112, "112"),
-        (12, "12"),
-        (13, "13"),
-        (141, "141"),
-        (142, "142"),
-        (143, "143"),
-        (21, "21"),
-        (22, "22"),
-        (23, "23"),
-        (24, "24"),
-        (31, "31"),
-        (32, "32"),
-        (33, "33"),
-        (41, "41"),
-        (42, "42"),
-        (43, "43"),
-        (51, "51"),
-        (52, "52"),
-    )
+    class TaskStatuses():
+        IN_PROGRESS = 0
+        FINISHED = 1
+        FAILED = 2
 
-    statuses = (
-        (0, "in progress"),
-        (1, "finished"),
-        (2, "failed"),
-    )
+        choises = (
+            (IN_PROGRESS, 'В работе'),
+            (FINISHED, 'Выполнено'),
+            (FAILED, 'Провалено'),
+        )
 
     title = models.CharField(unique=True, max_length=100)
-    paragraph = models.IntegerField(choices=paragraphs)
+    paragraph = models.IntegerField(choices=PARAGRAPHS)
     start = models.DateField(default=date.today)
     end = models.DateField()
-    status = models.IntegerField(choices=statuses, default=0)
+    status = models.IntegerField(choices=TaskStatuses.choises, default=0)
     description = models.TextField(blank=True)
 
     class Meta:
         ordering = ["end", "title"]
 
-    def set_status(self, status: int) -> NoReturn:
+    def set_status(self, status: int) -> None:
         if status not in (0, 1, 2):
-            raise ValueError("Статус должен быть в списке (0,1,2)")
+            raise ValueError("Некорректное значение статуса")
 
         self.status = status
         self.save()
--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,9 +1,12 @@
+from typing import Dict
+
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import reverse, get_object_or_404
 from django.views import View
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 from django.urls import reverse_lazy
+from django.db.models import QuerySet
 
 from apps.tasks.models import Task
 
@@ -12,12 +15,14 @@ class TaskCreate(CreateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("task-all")
+    template_name = "tasks/task/form.html"
 
 
 class TaskUpdate(UpdateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("task-all")
+    template_name = "tasks/task/form.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -35,8 +40,9 @@ class TaskDelete(View):
 
 class TaskList(ListView):
     model = Task
+    template_name = "tasks/task/list.html"
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         request_p = self.request.GET
 
         q = Task.objects
@@ -46,3 +52,9 @@ class TaskList(ListView):
             q = q.filter(status=status)
 
         return q.all()
+
+    def get_context_data(self, **kwargs) -> Dict:
+        context = super().get_context_data(**kwargs)
+        context['filter_status'] = self.request.GET.get("status")
+
+        return context
